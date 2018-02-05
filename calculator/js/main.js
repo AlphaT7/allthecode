9b2bbaea961a7b9272a9bc757d6e0005c22d367b
alphat7 <jccwinters@gmail.com>
2018-02-05 13:58:18 -0500

    calculator changes

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -1,4 +1,22 @@
+var main = {
+    vars: {
+        calc_array: [],
+        calc_str: 0,
+        flag: false
+    },
 
+    methods: {
+        build_str: function (char){
+            if (main.vars.flag == false){
+                main.vars.calc_str = char;
+                main.vars.flag = true;
+            } else {
+                main.vars.calc_str = main.vars.calc_str + char;
+            }
+            $('#calc_str').html(main.vars.calc_str);
+        }
+    }
+}
 
 $('body').keydown(function(e){
     switch (true){
@@ -114,6 +132,7 @@ $('body').keyup(function(e){
             $('#decimal').css("background-color","#555");
             $('#decimal').css("border-color","#000");
             console.log('decimal');
+            main.methods.build_str(".");
             break;
         case (e.which == 191 || e.which == 111):
             $('#divide').css("background-color","#555");
@@ -124,51 +143,61 @@ $('body').keyup(function(e){
             $('#zero').css("background-color","#555");
             $('#zero').css("border-color","#000");
             console.log('zero');
+            main.methods.build_str(0);
             break;
         case (e.which == 49 || e.which == 97):
             $('#one').css("background-color","#555");
             $('#one').css("border-color","#000");
             console.log('one');
+            main.methods.build_str(1);
             break;
         case (e.which == 50 || e.which == 98):
             $('#two').css("background-color","#555");
             $('#two').css("border-color","#000");
             console.log('two');
+            main.methods.build_str(2);
             break;
         case (e.which == 51 || e.which == 99):
             $('#three').css("background-color","#555");
             $('#three').css("border-color","#000");
             console.log('three');
+            main.methods.build_str(3);
             break;
         case (e.which == 52 || e.which == 100):
             $('#four').css("background-color","#555");
             $('#four').css("border-color","#000");
             console.log('four');
+            main.methods.build_str(4);
             break;
         case (e.which == 53 || e.which == 101):
             $('#five').css("background-color","#555");
             $('#five').css("border-color","#000");
             console.log('five');
+            main.methods.build_str(5);
             break;
         case (e.which == 54 || e.which == 102):
             $('#six').css("background-color","#555");
             $('#six').css("border-color","#000");
             console.log('six');
+            main.methods.build_str(6);
             break;
         case (e.which == 55 || e.which == 103):
             $('#seven').css("background-color","#555");
             $('#seven').css("border-color","#000");
             console.log('seven');
+            main.methods.build_str(7);
             break;
         case (e.which == 56 || e.which == 104):
             $('#eight').css("background-color","#555");
             $('#eight').css("border-color","#000");
             console.log('eight');
+            main.methods.build_str(8);
             break;
         case (e.which == 57 || e.which == 105):
             $('#nine').css("background-color","#555");
             $('#nine').css("border-color","#000");
             console.log('nine');
+            main.methods.build_str(9);
             break;
         default:
             console.log(e.which);
