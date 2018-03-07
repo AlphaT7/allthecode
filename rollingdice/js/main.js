3f5e95bfd8bb3bfadd96a1e3add18e4b3ddd9501
Jamison <jccwinters@gmail.com>
2018-03-07 13:19:41 -0500

    rolling dice works now

--- rollingdice/js/main.js
+++ rollingdice/js/main.js
@@ -1,76 +1,83 @@
-$('#roll').click(function(){
-    rolldice();
+var myChart = null;
+
+$("#roll").click(function() {
+  rolldice();
 });
 
-var rolldice = function(){
-    var dice = $('#dice').val();
-    var sides = $('#sides').val();
-    var roll = [];
-    var percent = {};
-    for (let i = 1; i <= sides; i++) {
-        percent[i] = 0;
+var rolldice = function() {
+  var dice = $("#dice").val();
+  var sides = $("#sides").val();
+  var roll = [];
+  var percent = {};
+  for (let i = dice; i <= sides * dice; i++) {
+    percent[i] = 0;
+  }
+  var percentage = [];
+  var bgcolor = function() {
+    let bgcarray = [];
+    for (let i = dice; i <= sides * dice; i++) {
+      bgcarray.push("rgba(54, 162, 235, 0.2)");
     }
-    var percentage = [];
-    var bgcolor = function(){
-        let bgcarray = [];
-        for (let i = 1; i <= sides; i++){
-            bgcarray.push('rgba(54, 162, 235, 0.2)');
-        }
-        return bgcarray;
-    };
-    bgbcolor = function(){
-        let bgbarray = [];
-        for (let i = 1; i <= sides; i++){
-            bgbarray.push('rgba(54, 162, 235, 1)');
-        }
-        return bgbarray;
-    };
-    chartlabels = function(){
-        let labels = [];
-        for (let i = dice; i <= sides*i; i++){
-            labels.push(i);
-        }
-        return labels;
+    return bgcarray;
+  };
+  bgbcolor = function() {
+    let bgbarray = [];
+    for (let i = dice; i <= sides * dice; i++) {
+      bgbarray.push("rgba(54, 162, 235, 1)");
     }
+    return bgbarray;
+  };
+  chartlabels = function() {
+    let labels = [];
+    for (let i = dice; i <= sides * dice; i++) {
+      labels.push(i);
+    }
+    return labels;
+  };
 
+  for (let i = 0; i < 1000; i++) {
+    let thisroll = getRandomInt(dice, sides * dice);
+    roll.push(thisroll);
+  }
+  //console.log(roll);
+  for (let j = dice; j <= sides * dice; j++) {
     for (let i = 0; i < 1000; i++) {
-        let thisroll = getRandomInt(dice,sides);
-        roll.push(thisroll);
-    }
-
-    for (let j = dice; j <= sides*j; j++) {
-      for (let i = 0; i < 1000; i++) {
-        if (roll[i] == j) {
-          percent[j] ++;
-        }
+      if (roll[i] == j) {
+        percent[j]++;
       }
     }
+  }
+  //console.log(percent);
+  for (let i = dice; i <= sides * dice; i++) {
+    percent[i] = percent[i] * 0.1;
+  }
 
-    for (let i = dice; i <= sides*i; i ++) {
-      percent[i] = percent[i] * .1;
-    }
+  for (let i = dice; i <= sides * dice; i++) {
+    percentage.push(percent[i]);
+  }
 
-    for (let i = dice; i <= sides; i ++) {
-        percentage.push(percent[i])
-    }
+  //console.log(percentage + '-test')
 
-    //console.log(percentage + '-test')
+  function getRandomInt(min, max) {
+    max = Number(max);
+    min = Number(min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-  
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            //labels: ["1", "2", "3", "4", "5", "6"],
-            labels: chartlabels(),
-            datasets: [{
-                label: '% for each side of the dice',
-                //data: [12, 19, 3, 5, 2, 3],
-                data: percentage,
-                /*
+  var ctx = document.getElementById("myChart").getContext("2d");
+  ctx.clearRect(0, 0, $("#mychart").width(), $("#mychart").height());
+  if (myChart != null) {myChart.destroy();}
+  myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      //labels: ["1", "2", "3", "4", "5", "6"],
+      labels: chartlabels(),
+      datasets: [
+        {
+          label: "% for each side of the dice",
+          //data: [12, 19, 3, 5, 2, 3],
+          data: percentage,
+          /*
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -80,8 +87,8 @@ var rolldice = function(){
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 */
-                backgroundColor: bgcolor(),
-                /*
+          backgroundColor: bgcolor(),
+          /*
                 borderColor: [
                     'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
@@ -91,19 +98,21 @@ var rolldice = function(){
                     'rgba(255, 159, 64, 1)'
                 ],
                 */
-                borderColor: bgbcolor(),
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
+          borderColor: bgbcolor(),
+          borderWidth: 1
         }
-    });
-  
-}
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+};
