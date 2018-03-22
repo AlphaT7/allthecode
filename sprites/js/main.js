0ed843a8908495289df13fb86813816f049e7c3b
Jamison <jccwinters@gmail.com>
2018-03-22 15:22:42 -0400

    update for pockethub

--- sprites/js/main.js
+++ sprites/js/main.js
@@ -4,12 +4,46 @@ const $ = q => {
 
 const knightImage = new Image();
 knightImage.src = "./img/knights.png";
-knightImage.onload = function() {
-  defaultKnight.render();
+knightImage.onload = () => {
+  downMidPos.render();
 };
 
-const sprite = function(options) {
-  var that = {};
+const gameloop = (dir) => {
+    
+    switch (dir){
+        case 'left':
+            downLeftPos.render();
+            dir = 'mid';
+            break;
+        case 'mid':
+
+            break;
+    }
+    
+    if(dir == 'left') {
+      downLeftPos.render();
+      dir = 'mid';
+    }
+    window.requestAnimationFrame(gameloop);
+};
+
+const sprite = options => {
+  let that = {},
+    rameIndex = 0,
+    tickCount = 0;/*,
+    ticksPerFrame = 0,
+    numberOfFrames = options.numberOfFrames || 1;*/
+
+  that.update = () => {
+    tickCount += 1;
+
+    if (tickCount >= ticksPerFrame) {
+      tickCount = 0;
+
+      // Go to the next frame
+      frameIndex += 1;
+    }
+  };
 
   that.context = options.context;
   that.width = options.width;
@@ -24,7 +58,7 @@ const sprite = function(options) {
   that.dw = options.dw;
   that.dh = options.dh;
 
-  that.render = function() {
+  that.render = () => {
     that.context.clearRect(0, 0, canvas.width, canvas.height);
     // Draw the animation
     that.context.drawImage(
@@ -39,15 +73,34 @@ const sprite = function(options) {
       that.dh
     );
   };
-  
+
   return that;
 };
+
+var flag = false;
+var tickCount = 0;
+var ticksPerFrame = 10;
+var dir;
+var prevDir;
 
 var canvas = $("#knightAnimation");
 canvas.width = 80;
 canvas.height = 120;
 
-var defaultKnight = sprite({
+var downLeftPos = sprite({
+  context: canvas.getContext("2d"),
+  image: knightImage,
+  sx: 0,
+  sy: 0,
+  sw: 77,
+  sh: 120,
+  dx: 0,
+  dy: 0,
+  dw: 77,
+  dh: 120
+});
+
+var downMidPos = sprite({
   context: canvas.getContext("2d"),
   image: knightImage,
   sx: 77,
@@ -60,13 +113,47 @@ var defaultKnight = sprite({
   dh: 120
 });
 
-$("body").addEventListener("keyup", function(e) {
-  switch (e) {
+var downRightPos = sprite({
+  context: canvas.getContext("2d"),
+  image: knightImage,
+  sx: 154,
+  sy: 0,
+  sw: 77,
+  sh: 120,
+  dx: 0,
+  dy: 0,
+  dw: 77,
+  dh: 120
+});
+
+$("body").addEventListener("keydown", function(e) {
+  switch (e.keyCode) {
     case 38:
       console.log("up");
       break;
     case 40:
-      console.log("down");
+      flag = true;
+      gameloop('down');
+      break;
+    case 37:
+      console.log("left");
+      break;
+    case 39:
+      console.log("right");
+      break;
+    default:
+      console.log("other");
+      break;
+  }
+});
+
+$("body").addEventListener("keyup", function(e) {
+  switch (e.keyCode) {
+    case 38:
+      console.log("up");
+      break;
+    case 40:
+      flag = false;
       break;
     case 37:
       console.log("left");
