12bf0be8bbceb8dc1c138bc579e7a10ecaeaf9ed
Jamison <jccwinters@gmail.com>
2018-03-31 02:04:30 -0400

    condensed main.js and removed redundant functions

--- hexagongrid/js/main.js
+++ hexagongrid/js/main.js
@@ -2,16 +2,8 @@ const $ = q => {
   return document.querySelector(q);
 };
 
-Array.prototype.clone = function() {
-  return this.slice(0);
-};
-
 const c = d => {
   console.log(d);
-};
-
-const heightDifference = size => {
-  return Math.floor(Math.sqrt(3) / 2 * size) * 2;
 };
 
 var canvas = $("#hexgrid");
@@ -27,18 +19,15 @@ var mouse = {
 };
 var hexArr = [];
 var highlighted = [];
-var flag = true;
-//var prevHighlighted = {};
-canvas.width = 1000;
+canvas.width = 820;
 canvas.height = 600;
 canvas.addEventListener("mousemove", e => {
   var rect = canvas.getBoundingClientRect();
   mouse.x = e.clientX - rect.left;
   mouse.y = e.clientY - rect.top;
 
-  // clone hexArr and find the hex that is moused over
-  let newHexArr = hexArr.clone();
-  let center = newHexArr.filter(hex => {
+  // filter hexArr find the hex that is moused over
+  let center = hexArr.filter(hex => {
     if (
       mouse.x <= hex.x + hexSize &&
       mouse.y <= hex.y + hexSize &&
@@ -73,45 +62,45 @@ canvas.addEventListener("mousemove", e => {
       highlighted.map(element => {
         element.c = "#F4F4F1";
       });
-      newGrid.highlight(highlighted);
+      newGrid.draw(highlighted);
     }
 
     // find the equation of the line (y = mx + b), and then test each coordinate against the equation to see if it solves it.
     // if it does, than it returns those hex coordinates.
-    highlighted = newHexArr.filter(hex => {
+    highlighted = hexArr.filter(hex => {
       let m = (hex2.y - center.y) / (hex2.x - center.x);
       let b = center.y - m * center.x;
 
       if (hex.y == Math.round(m * hex.x + b)) {
-        hex.c = "blue";
+        hex.c = "#75CAEB";
         return hex;
       }
     });
 
     highlighted = highlighted.concat(
-      newHexArr.filter(hex => {
+      hexArr.filter(hex => {
         let m = (hex3.y - center.y) / (hex3.x - center.x);
         let b = center.y - m * center.x;
 
         if (hex.y == Math.round(m * hex.x + b)) {
-          hex.c = "green";
+          hex.c = "#5CB85C";
           return hex;
         }
       })
     );
 
     highlighted = highlighted.concat(
-      newHexArr.filter(hex => {
+      hexArr.filter(hex => {
         if (hex.x == center.x) {
-          hex.c = "red";
+          hex.c = "#FF847D";
           return hex;
         }
       })
     );
 
-    newGrid.highlight(highlighted);
-    center.c = "orange";
-    newGrid.highlight([center]);
+    newGrid.draw(highlighted);
+    center.c = "#F0AD4E";
+    newGrid.draw([center]);
   }
 });
 
@@ -126,7 +115,7 @@ const drawGrid = function(size) {
     };
   };
 
-  this.highlight = (thisArr, color) => {
+  this.draw = thisArr => {
     thisArr.forEach(element => {
       let s1 = this.hexPoint(element, size, 1),
         s2 = this.hexPoint(element, size, 2),
@@ -159,48 +148,13 @@ const drawGrid = function(size) {
     });
   };
 
-  this.drawHex = (center, size) => {
-    //let center = { x: 200, y: 200 },
-    if (flag) {
-      hexArr.push(center);
-    }
-    let s1 = this.hexPoint(center, size, 1),
-      s2 = this.hexPoint(center, size, 2),
-      s3 = this.hexPoint(center, size, 3),
-      s4 = this.hexPoint(center, size, 4),
-      s5 = this.hexPoint(center, size, 5),
-      s6 = this.hexPoint(center, size, 6);
-
-    ctx.beginPath();
-    ctx.moveTo(s1.x, s1.y);
-    ctx.lineTo(s2.x, s2.y);
-    ctx.lineTo(s3.x, s3.y);
-    ctx.lineTo(s4.x, s4.y);
-    ctx.lineTo(s5.x, s5.y);
-    ctx.lineTo(s6.x, s6.y);
-    ctx.lineTo(s1.x, s1.y);
-    ctx.fillStyle = center.c;
-    ctx.fill();
-
-    ctx.strokeStyle = "#C6CDCA";
-    ctx.beginPath();
-    ctx.moveTo(s1.x, s1.y);
-    ctx.lineTo(s2.x, s2.y);
-    ctx.lineTo(s3.x, s3.y);
-    ctx.lineTo(s4.x, s4.y);
-    ctx.lineTo(s5.x, s5.y);
-    ctx.lineTo(s6.x, s6.y);
-    ctx.lineTo(s1.x, s1.y);
-    ctx.stroke();
-  };
-
   this.setCoordinates = (start, size) => {
     let origin = {
       x: start.x,
       y: start.y
     };
 
-    let sizeMultiplier = 18;
+    let sizeMultiplier = 15;
     for (let j = 0; j < 34; j++) {
       for (let i = start.x; i < size * sizeMultiplier; i += size) {
         hexArr.push({ x: start.x, y: start.y, c: "#F4F4F1" });
@@ -215,14 +169,8 @@ const drawGrid = function(size) {
     }
     flag = false;
   };
-
-  this.draw = function(size) {
-    hexArr.forEach(value => {
-      this.drawHex(value, size);
-    });
-  };
 };
 
 var newGrid = new drawGrid(hexSize);
 newGrid.setCoordinates({ x: hexSize, y: hexSize }, hexSize);
-newGrid.draw(hexSize);
+newGrid.draw(hexArr);
